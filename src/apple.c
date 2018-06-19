464eabf29b9612fb88f83116cf3d28054951efde
Kazuya-Kawahara <kawahara.k.ad@m.titech.ac.jp>
2018-06-19 17:29:10 +0900

    appleFinished

--- src/apple.c
+++ src/apple.c
@@ -1,17 +1,37 @@
+
 #include <stdio.h>
 
 int n;
 int k;
 int A[100000];
+int i;
 
+int p(int m){
+    int s;
+    s = 0;
+    for(i = 0; i < n; i++){
+        s = s + (A[i]+m-1)/m;
+    }
+    return s<=k;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    lb = 1;
+    ub = (1000000000+(k/n)-1)/(k/n);
+    while(ub-lb > 1){
+        int m = (ub + lb) / 2;
+        if(p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    printf("%d\n", ub);
+    return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -3,6 +3,16 @@
 int n;
 int k;
 int A[100000];
+int i;
+
+int p(int m){
+    int s;
+    s = 0;
+    for(i = 0; i < n; i++){
+        s = s + A[i]/m
+    }
+    return s>=k
+}
 
 
 int main(){
@@ -11,6 +21,19 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  
+  lb = 0;
+  ub = 1000000000/(k/n);
+  while(ub-lb > 1){
+      int m = (ub + lb) / 2;
+      if(p(m)){
+          lb = m;
+      }
+      else {
+          ub = m;
+      }
+  }
+  printf("%d\n", lb);
 
 
   return 0;
