de3ba7964dd0c8624d1748a40b9edefe8f299b6a
Kazuya-Kawahara <kawahara.k.ad@m.titech.ac.jp>
2018-06-19 18:51:18 +0900

    works途中

--- src/works.c
+++ src/works.c
@@ -3,7 +3,26 @@
 int n;
 int k;
 int A[100000];
+int i;
 
+int p(int m){
+    int j;
+    int sum;
+    i = 1;
+    j = 0;
+    while(i < n){
+        sum = 0;
+        i = i - 1;
+        while(sum <= m){
+            while(A[i] > 0){
+                sum = sum + A[i];
+                i = i + 1;
+            }
+        }
+        j = j + 1;
+    }
+    return j <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +30,19 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
+    
+  lb = n/k;
+  ub = 1000000000;
+  while(ub-lb > 1){
+      int m = (ub + lb) / 2;
+      if(p(m)){
+          ub = m;
+      }
+      else {
+          lb = m;
+      }
+  }
+  printf("%d\n", ub);
 
   return 0;
 }
